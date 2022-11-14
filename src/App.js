2960896ce5e7786fc6b8d2943d976107ab94a77b
unknown <rafalchrzanowski00@gmail.com>
2022-11-14 22:00:04 +0100

    First

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <main>
+    <div className="App">
+      <header></header>
+      <div className="AppBody">
+
+      </div>
+    </div>
+  </main>
+  );
+}
+
+export default App;
